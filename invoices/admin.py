d1dd4c3b22c52a5eb24e2c6412c0d56b05a55f7d
mehdi <mehdi.benammar@pragmaconsult.lu>
2013-09-19 21:01:00 +0200

    ordering by name is possible nowfor patients

--- invoices/admin.py
+++ invoices/admin.py
@@ -2,28 +2,22 @@ from django.contrib import admin
 from invoices.models import CareCode, Prestation, Patient
 
 
-# class PrestationsInline(admin.TabularInline):
-#     model = Prestation
-#     extra = 2
-# 
-
 class CareCoreAdmin(admin.ModelAdmin):
     list_display = ('code', 'name', 'gross_amount', 'net_amount' , 'is_private')
     search_fields = ['code', 'name']
+
+class PatientAdmin(admin.ModelAdmin):
+    list_filter = ('city', )
+    list_display = ('name', 'first_name', 'phone_number','code_sn')
+    search_fields = ['name', 'first_name', 'code_sn']
     
 class PrestationAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_display = ('patient', 'carecode', 'date')
     search_fields = ['patient__name', 'patient__first_name']
-#     fieldsets = [
-#         (None,               {'fields': ['patient', 'carecode']}),
-#         ('Date information', {'fields': ['date'], 'classes': ['collapse']}),
-#     ]
-#     inlines = [PrestationsInline]
-
 
     
 admin.site.register(CareCode, CareCoreAdmin)
 admin.site.register(Prestation, PrestationAdmin)
-admin.site.register(Patient)
+admin.site.register(Patient, PatientAdmin)
 
