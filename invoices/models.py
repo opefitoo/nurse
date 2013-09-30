87f2dd5e0089a1b03b59aaf1d8b7270f5d349973
mehdi <mehdi.benammar@pragmaconsult.lu>
2013-09-30 23:13:17 +0200

    adding feature of automatic add for invoice item

--- invoices/models.py
+++ invoices/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from setuptools.tests.doctest import is_private
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
 
 # Create your models here.
 class CareCode(models.Model):
     code = models.CharField(max_length=30)
     name = models.CharField(max_length=50)
-    description = models.TextField(max_length=200)
+    description = models.TextField(max_length=100)
     # prix net = 88% du montant brut
     # prix brut
     gross_amount = models.DecimalField("montant brut", max_digits=5, decimal_places=2)
     
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.code + ":" +self.name 
+        return '%s: %s' % (self.code, self.name) 
     
 class Patient(models.Model):
     code_sn = models.CharField(max_length=30)
@@ -23,12 +27,21 @@ class Patient(models.Model):
     phone_number = models.CharField(max_length=30)
     participation_statutaire = models.BooleanField()
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.name + " " + self.first_name
+        return '%s %s' % (self.first_name, self.last_name)
+    
+class InvoiceItem(models.Model):
+    invoice_number = models.CharField(max_length = 50)
+    date = models.DateField('Invoice date')
+    invoice_sent = models.BooleanField()
+    invoice_paid = models.BooleanField()
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return 'invoice# %s' % (self.invoice_number)
     
 class Prestation(models .Model):
     patient = models.ForeignKey(Patient)
     carecode = models.ForeignKey(CareCode)
     date = models.DateTimeField('date')
+    invoice_item = models.ForeignKey(InvoiceItem)
     @property
     def net_amount(self):
         "Returns the net amount"
@@ -36,6 +49,24 @@ class Prestation(models .Model):
             return self.carecode.gross_amount
         return ((self.carecode.gross_amount * 88) / 100 )
     
+    def save(self, *args, **kwargs):
+        q = InvoiceItem.objects.filter(invoice_paid = False).select_related()        
+        q.filter(prestation__patient = self.patient)
+        if not q:
+            newInvoice = InvoiceItem.objects.create(invoice_number = len(self.patient.name),
+                                       date = datetime.date.today(),
+                                       invoice_sent = False,
+                                       invoice_paid = False)
+            newInvoice.save()
+            invoice_item = newInvoice
+            print "***** q is emtpy"
+            super(Prestation, self).save(*args, **kwargs) # Call the "real" save() method.        
+        else:
+            print "**** q is  %s" % q
+            super(Prestation, self).save(*args, **kwargs) # Call the "real" save() method.
+    
     def __unicode__(self):  # Python 3: def __str__(self):
-        return "code:" + self.carecode.code + "- nom patient:" + self.patient.name
+        return 'code: %s - nom patient: %s' % (self.carecode.code , self.patient.name)
+
+    
     
---
+++ invoices/tests.py
@@ -0,0 +1,81 @@
+from django.test import TestCase
+from invoices.models import CareCode, Patient, Prestation
+
+# Create your models here.
+class CareCodeTestCase(TestCase):
+    def setUp(self):
+        CareCode.objects.create(code ='code1', 
+                               name= "code1", 
+                               description= "descr", 
+                               gross_amount = 10)
+        Patient.objects.create(code_sn ='codesn')
+        Prestation.objects.create(patient =  Patient.objects.get(code_sn = 'codesn'), 
+                                  carecode = CareCode.objects.get(pk=1))
+    def test_simple_creation(self):
+        code1 = CareCode.objects.get(name = "code1")
+        self.assertEqual(code1.code, "code1")
+        codes = CareCode.objects.all()
+        self.assertEqual(len(list(codes)), 1)
+        patients = Patient.objects.all()
+        self.assertEqual(len(list(patients)), 1)
+        
+        prests = Prestation.objects.all()
+        self.assertEqual(len(list(prests)), 1)
+        
+    def test_failing_test(self):
+        self.assertFalse(False, "msg")
+#     code = models.CharField(max_length=30)
+#     name = models.CharField(max_length=50)
+#     description = models.TextField(max_length=100)
+#     # prix net = 88% du montant brut
+#     # prix brut
+#     gross_amount = models.DecimalField("montant brut", max_digits=5, decimal_places=2)
+#     
+#     def __unicode__(self):  # Python 3: def __str__(self):
+#         return '%s: %s' % (self.code, self.name) 
+#     
+# class Patient(models.Model):
+#     code_sn = models.CharField(max_length=30)
+#     first_name = models.CharField(max_length=30)
+#     name = models.CharField(max_length=30)
+#     address = models.TextField(max_length=30)
+#     zipcode = models.CharField(max_length=10)
+#     city = models.CharField(max_length= 30)
+#     phone_number = models.CharField(max_length=30)
+#     participation_statutaire = models.BooleanField()
+#     def __unicode__(self):  # Python 3: def __str__(self):
+#         return '%s %s' % (self.first_name, self.last_name)
+#     
+# class Prestation(models .Model):
+#     patient = models.ForeignKey(Patient)
+#     carecode = models.ForeignKey(CareCode)
+#     date = models.DateTimeField('date')
+#     #invoice_item = models.ForeignKey(InvoiceItem)
+#     @property
+#     def net_amount(self):
+#         "Returns the net amount"
+#         if not self.patient.participation_statutaire:
+#             return self.carecode.gross_amount
+#         return ((self.carecode.gross_amount * 88) / 100 )
+#     
+#     def save(self, *args, **kwargs):
+#         q = InvoiItem.objects.filter(__invoice_paid__ != True)
+#         q.filter(__patient__code_sn__ == self.patient.code_sn)
+#         
+#         if self.name == "Yoko Ono's blog":
+#             return # Yoko shall never have her own blog!
+#         else:
+#             super(Blog, self).save(*args, **kwargs) # Call the "real" save() method.
+#     
+#     def __unicode__(self):  # Python 3: def __str__(self):
+#         return 'code: %s - nom patient: %s' % (self.carecode.code , self.patient.name)
+#     
+# class InvoiceItem(models.Model):
+#     invoice_number = models.CharField(max_length = 50)
+#     date = models.DateField('Invoice date')
+#     invoice_sent = models.BooleanField()
+#     invoice_paid = models.BooleanField()
+#     def __unicode__(self):  # Python 3: def __str__(self):
+#         return 'invoice# %s' % (self.invoice_number)
+#     
+#     
