68af6f738644cd601e192fe4c6e2471ebb392ebd
mehdi <mehdi.benammar@pragmaconsult.lu>
2013-11-05 02:10:12 +0100

    remove views from urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -16,7 +16,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pdf_report/', views.pdf_report, name='pdf_report'),
      # include the lookup urls
     url(r'^admin/lookups/', include(ajax_select_urls)),
     url(r'^admin/', include(admin.site.urls)),
